e51252eafef6a9cb5343a4d45d76e169e1567927
Yaqub Tawab Olamilekan <maktee2002@gmail.com>
2025-05-05 14:23:04 +0100

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,18 @@ import type React from "react"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import { AuthProvider } from "@/contexts/auth-context"
+import { AuthProvider } from "@/contexts/auth-context";
+import Logo1 from "../public/logo.png";
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Gibraltar Private Bank & Trust",
   description: "Secure online banking for Gibraltar Private Bank & Trust clients",
-    generator: 'v0.dev'
+    generator: 'v0.dev',
+    icons: {
+      icon: "../public/logo.png",
+    },
 }
 
 export default function RootLayout({
